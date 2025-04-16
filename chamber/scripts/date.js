d9dbe0266eb059efcf5b76db91146144a1b575de
jootaef <jaydanv1008@gmail.com>
2025-04-16 01:30:43 -0500

    2

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,6 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("year").textContent = new Date().getFullYear();
-
+  
     let lastModified = document.lastModified;
     document.getElementById("lastModified").textContent = "Last Update: " + lastModified;
-});
+  
+    const visitMsgEl = document.getElementById("visit-msg");
+    const lastVisit = localStorage.getItem("lastVisit");
+  
+    if (lastVisit) {
+      visitMsgEl.textContent = `Welcome back! Your last visit was on ${lastVisit}.`;
+    } else {
+      visitMsgEl.textContent = "Welcome! This is your first visit.";
+    }
+  
+    localStorage.setItem("lastVisit", new Date().toLocaleString());
+  });
+  
